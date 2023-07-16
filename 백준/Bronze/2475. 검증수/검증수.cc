91f69b22872b70962f0f055e16e2631e54e6af45
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-07-16 22:26:51 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, sum(0);
+
+    for (int i = 0; i < 5; i++) {
+        cin >> n;
+        sum += n * n;
+    }
+
+    cout << (sum % 10);
+}
