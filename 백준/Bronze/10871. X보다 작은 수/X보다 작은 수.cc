ba36085217f35292b88f365f2c570af75de7fa7d
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-07-17 14:37:37 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, x;
+    int a[10000];
+
+    cin >> n >> x;
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < n; i++)
+        if (a[i] < x)
+            cout << a[i] << " ";
+}
