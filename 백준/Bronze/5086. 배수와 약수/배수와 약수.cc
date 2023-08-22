a962a28ff58fa0f8f399a9cf43624ad8ba3f338a
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-08-22 16:17:45 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    while (true) {
+        int a, b;
+        cin >> a >> b;
+        
+        if (a == 0 && b == 0)
+            break;
+        if (a == 0 || b == 0)
+            cout << "neither" << '\n';
+        
+        if (b % a == 0)
+            cout << "factor" << '\n';
+        else if (a % b == 0)
+            cout << "multiple" << '\n';
+        else
+            cout << "neither" << '\n';
+    }
+}
