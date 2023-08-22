c5a0557b09283ef7588386f27520880b4aad41ac
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-08-22 15:34:31 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x;
+    cin >> x;
+
+    int i = 1;
+    while (x > i) {
+        x -= i;
+        i++;
+    }
+
+    if (i % 2 == 1)
+        cout << i + 1 - x << '/' << x;
+    else
+        cout << x << '/' << i + 1 - x;
+}
