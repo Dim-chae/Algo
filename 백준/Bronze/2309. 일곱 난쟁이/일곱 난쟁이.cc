7fae9b2a886a4e4d5b430b82b5f303cb8b85754b
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-12-28 16:43:13 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int arr[9];
+    int total = 0;
+
+    for (int i = 0; i < 9; i++) {
+        cin >> arr[i];
+        total += arr[i];
+    }
+
+    sort(arr, arr + 9);
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = i + 1; j < 9; j++) {
+            if (total - arr[i] - arr[j] == 100) {
+                for (int k = 0; k < 9; k++) {
+                    if (k != i && k != j) {
+                        cout << arr[k] << '\n';
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
