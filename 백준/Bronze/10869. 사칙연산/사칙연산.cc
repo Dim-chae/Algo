7fde1b3a738d3f73661a1125c3a6c514fc13dae4
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-12-31 16:07:00 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int A, B;
+    cin >> A >> B;
+
+    cout << A + B << '\n' << A - B << '\n' << A * B << '\n' << A / B << '\n' << A % B;
+}
