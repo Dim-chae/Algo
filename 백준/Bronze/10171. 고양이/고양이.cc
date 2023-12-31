f5d16a44f2f31f1e1097801e21608e0e9638bf70
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2023-12-31 16:02:51 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  
+  cout << "\\    /\\" << '\n';
+  cout << " )  ( \')" << '\n';
+  cout << "(  /  )" << '\n';
+  cout << " \\(__)|";
+}
