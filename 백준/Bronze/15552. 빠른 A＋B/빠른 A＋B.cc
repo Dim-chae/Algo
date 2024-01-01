3b291cd44d9f838ef49ca64c9bebc9f2b7035fc1
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-01-01 16:35:30 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 216 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int a, b, t;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
