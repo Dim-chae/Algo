0b363b062324782e613f87e891fc1d4a5d74139a
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-01-06 19:09:08 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -3,41 +3,28 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
     cin.tie(0);
+    ios::sync_with_stdio(0);
 
-    int n;
-    cin >> n;
+    int N;
+    cin >> N;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++)
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < i; j++)
             cout << '*';
-
-        for (int j = n - 1; j > i; j--)
+        for (int j = (N - i) * 2; j > 0; j--)
             cout << ' ';
-
-        for (int j = n - 1; j > i; j--)
-            cout << ' ';
-
-        for (int j = 0; j < i + 1; j++)
+        for (int j = 0; j < i; j++)
             cout << '*';
-
-        cout << '\n';
+    cout << '\n';
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = n - 1; j > i; j--)
+    for (int i = 1; i < N; i++) {
+        for (int j = N - i; j > 0; j--)
             cout << '*';
-
-        for (int j = 1; j < i + 2; j++)
+        for (int j = 0; j < 2 * i; j++)
             cout << ' ';
-
-        for (int j = 1; j < i + 2; j++)
-            cout << ' ';
-
-        for (int j = n - 1; j > i; j--)
+        for (int j = N - i; j > 0; j--)
             cout << '*';
-
-        cout << '\n';
+    cout << '\n';
     }
 }
