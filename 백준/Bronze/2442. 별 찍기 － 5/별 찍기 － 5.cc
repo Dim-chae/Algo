35e3816e0f7845bb45225113a33384d736a8ae35
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-01-06 17:47:09 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
     cin.tie(0);
+    ios::sync_with_stdio(0);
 
-    int n;
-    cin >> n;
+    int N;
+    cin >> N;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = n - i; j > 0; j--)
+    for (int i = 1; i <= N; i++) {
+        for (int j = N - i; j > 0; j--)
             cout << ' ';
-        for (int j = 1; j <= 2 * i - 1; j++)
+        for (int j = 1; j <= i * 2 - 1; j++)
             cout << '*';
         cout << '\n';
     }
