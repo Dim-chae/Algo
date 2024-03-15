08af7441b7d0716387c4295566e389af8ea2afb2
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-03-16 06:04:58 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2232 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int board[101][101];
+int dist[101][101];
+
+#define X first
+#define Y second 
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    char c;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> c;
+            board[i][j] = c - '0';
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        fill(dist[i], dist[i] + M, -1);
+    }
+
+    queue<pair<int, int>> Q;
+
+    dist[0][0] = 0;
+    Q.push({0, 0});
+
+    while (!Q.empty()) {
+        pair<int, int> cur = Q.front(); Q.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (dist[nx][ny] >= 0 || board[nx][ny] != 1) continue;
+            
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            Q.push({nx, ny});
+        }
+    }
+    cout << dist[N-1][M-1] + 1;
+}
