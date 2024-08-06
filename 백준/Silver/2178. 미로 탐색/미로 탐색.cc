8805be564d2e477b575f24797fb99543fa8602fe
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-08-06 13:23:31 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int board[101][101];
-int dist[101][101];
-
 #define X first
-#define Y second 
+#define Y second
 
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
+string board[102];
+int dist[102][102];
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 int main() {
     cin.tie(0);
@@ -18,23 +18,17 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    char c;
+    for (int i = 0; i < N; i++)
+        cin >> board[i];
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> c;
-            board[i][j] = c - '0';
-        }
-    }
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
         fill(dist[i], dist[i] + M, -1);
-    }
 
     queue<pair<int, int>> Q;
 
-    dist[0][0] = 0;
     Q.push({0, 0});
-
+    dist[0][0] = 0;
+      
     while (!Q.empty()) {
         pair<int, int> cur = Q.front(); Q.pop();
 
@@ -43,11 +37,11 @@ int main() {
             int ny = cur.Y + dy[dir];
 
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-            if (dist[nx][ny] >= 0 || board[nx][ny] != 1) continue;
+            if (board[nx][ny] != '1' || dist[nx][ny] >= 0) continue;
             
             dist[nx][ny] = dist[cur.X][cur.Y] + 1;
             Q.push({nx, ny});
         }
     }
-    cout << dist[N-1][M-1] + 1;
+    cout << dist[N - 1][M - 1] + 1;
 }
