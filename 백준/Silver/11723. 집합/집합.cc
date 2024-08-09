cb635e08fde1e88554aab3ecb0c11249920f96c0
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-08-09 11:05:30 +0900

    [Silver V] Title: 집합, Time: 564 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -2,18 +2,21 @@
 
 using namespace std;
 
-vector<int> v;
+bool S[21];
 
-bool check(int x) {
-    auto it = find(v.begin(), v.end(), x);
-    if (it == v.end()) return 0;
-    else return 1;
+bool check(int x){
+    if (S[x]) return 1;
+    else return 0;
 }
 
-void all() {
-    v.clear();
+void all(){
     for (int i = 0; i < 20; i++)
-        v.push_back(i + 1);
+        S[i + 1] = 1;
+}
+
+void empty(){
+    for (int i = 0; i < 20; i++)
+        S[i + 1] = 0;
 }
 
 int main() {
@@ -27,30 +30,27 @@ int main() {
         string s;
         cin >> s;
 
+        int x;
+
         if (s == "add") {
-            int x;
             cin >> x;
-            if (!check(x)) v.push_back(x);
+            if (!check(x)) S[x] = 1;
         }
         else if (s == "remove") {
-            int x;
             cin >> x;
-            if (check(x)) v.erase(remove(v.begin(), v.end(), x));
+            if (check(x)) S[x] = 0;
         }
         else if (s == "check") {
-            int x;
             cin >> x;
             cout << check(x) << '\n';
         }
         else if (s == "toggle") {
-            int x;
             cin >> x;
-            if (check(x)) v.erase(remove(v.begin(), v.end(), x));
-            else v.push_back(x);
+            S[x] = !check(x);
         }
         else if (s == "all")
             all();
         else if (s == "empty")
-            v.clear();
+            empty();
     }
 }
