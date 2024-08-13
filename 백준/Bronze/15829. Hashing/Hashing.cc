7152b3332524315edb5a1fa0aff0999cf8668a25
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-08-13 14:47:28 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -9,12 +9,16 @@ int main() {
     int L;
     cin >> L;
 
+    long long int M = 1234567891;
+    long long int r = 1;
     long long int total = 0;
+ 
+    string s;
+    cin >> s;
 
     for (int i = 0; i < L; i++) {
-        char tmp;
-        cin >> tmp;
-        total += (tmp - 'a' + 1) * pow(31, i);
+        total += (s[i] - 96) * r % M;
+        r = r * 31 % M;
     }
-    cout << total % 1234567891;
+    cout << total % M;
 }
