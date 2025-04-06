059e5c9a1e3d66aed10c3bd9d5a5f97687c0cc8e
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2025-04-06 22:33:46 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    cout << (B / A) * C * 3;
+}
