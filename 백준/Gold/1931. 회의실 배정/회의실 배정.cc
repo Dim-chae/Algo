700a8d2f06971992fe7d35e2d4b5b87d20d23ac3
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2025-04-08 21:24:47 +0900

    [Gold V] Title: 회의실 배정, Time: 32 ms, Memory: 3964 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define end first
+#define start second
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+
+    int s, e, cnt = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    queue<pair<int, int>> q;
+
+    while (N--) {
+        cin >> s >> e;
+        pq.push({e, s});        
+    }
+
+    while (!pq.empty()) {
+        if (q.empty() || q.back().end <= pq.top().start)
+            q.push({pq.top().end, pq.top().start});
+        pq.pop();
+    }
+    
+    cout << q.size();
+}
