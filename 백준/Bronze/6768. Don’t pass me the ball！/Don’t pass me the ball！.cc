e2147b45ca7ecbc343592ba2ca5b0c7b46d76613
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2025-04-08 10:34:34 +0900

    [Bronze III] Title: Don’t pass me the ball!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6768. Don’t pass me the ball！/Don’t pass me the ball！.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int J;
+    cin >> J;
+    
+    cout << ((J-3) * (J-2) * (J-1)) / 6;
+}
