bf122561de531f9c3896dac11bded7cdaf310482
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2025-04-23 13:39:05 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int c = 1;
+    
+    while (1) {
+        int N, M;
+        cin >> N;
+        if (N == 0) break;
+        
+        while (N--) cin >> M;
+        cout << "Case " << c++ << ": Sorting... done!" << '\n';
+    }
+    
+}
