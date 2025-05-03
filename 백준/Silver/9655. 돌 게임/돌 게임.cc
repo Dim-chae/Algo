4a9d5cee3a98bd80fcda135fa408b04ebb515991
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2025-05-04 00:01:40 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+
+    if (N % 2 == 1) cout << "SK";
+    else cout << "CY";
+}
