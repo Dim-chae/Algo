8c359a76d090425addff91db1b12f98b504ecd18
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2025-05-16 14:41:43 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int paper[128][128];
+int white = 0, blue = 0;
+
+void check(int x, int y, int n) {
+    int color = paper[x][y];
+    for (int i = x; i < x + n; i++)
+        for (int j = y; j < y + n; j++)
+            if (paper[i][j] != color) {
+                check(x, y, n / 2);
+                check(x + n / 2, y, n / 2);
+                check(x, y + n / 2, n / 2);
+                check(x + n / 2, y + n / 2, n / 2);
+                return;
+            }
+    if (color == 0) white++;
+    else blue++;
+}   
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> paper[i][j];
+        
+    check(0, 0, N);
+    cout << white << '\n' << blue;
+}
